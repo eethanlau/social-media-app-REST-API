9a87aa5e13b05de3752c0af3e871bc8dd9261e28
eethanlau <ethan.lau151@gmail.com>
2023-07-21 17:38:52 -0400

    begin routes

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
-app.listen(8800, () => {
+dotenv.config();
+
+//Mongoose connection
+
+mongoose.connect(process.env.MONGO_URL, {});
+
+app.get("/", (req,res) => {
+  res.send("welcome to the homepage")
+})
+
+app.get("/users", (req,res) => {
+  res.send("welcome to the user page")
+})
+
+//Middleware for project
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(3000, () => {
   console.log("Backend server is running!");
 });
